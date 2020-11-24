422b780aed4d4f2e79bdcf678a35dd3ca5bcc958
matheus <mmartins.oliveira@outlook.com>
2020-11-24 14:41:21 -0300

    new commit

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 
 import Header from '../../components/Header/index'
 
-import Main from '../../components/Main/index'
-
 const DashBoard = () => {
 
 
     return (
         <>
          <Header />
-         <h1 style={{textAlign:"center", marginTop: 60}} > Portal do Walter </h1>
-         <Main />
+         <h1 style={{textAlign:"center", marginTop: 60}} > Portal  </h1>
         </>
     )
 }
